8dfdadc93b59f8f92cb37d19d61b3225cb87a8f1
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-12 20:33:26 +0900

    chore: Standardize code formatting and indentation across configuration files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import {css} from "styled-system/css";
+import { css } from "styled-system/css";
 
 export default function Home() {
-  return (
-    <div >
-      <main>
-              <div className={ css({
-                bg: "red",
-              })}>aa</div>
-      </main>
-      <footer>
-
-      </footer>
-    </div>
-  );
+    return (
+        <div>
+            <main>
+                <div
+                    className={css({
+                        bg: "red",
+                    })}
+                >
+                    aa
+                </div>
+            </main>
+            <footer></footer>
+        </div>
+    );
 }
