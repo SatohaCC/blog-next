b47213e0eb764125335283dc023170482d19927f
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-12 20:55:18 +0900

    chore: Add Prettier configuration to ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+// Prettierを含めたESLintの設定
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript", "prettier")];
 
 export default eslintConfig;
