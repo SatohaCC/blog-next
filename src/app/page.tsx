50093404caff142b7848dcc3aaf2252a22a87b81
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-13 22:56:58 +0900

    feat: Add Button component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+import Button from "@/components/ui/Button";
 import { css } from "styled-system/css";
 
 export default function Home() {
     return (
         <div>
             <main>
-                <div
-                    className={css({
-                        bg: "red",
-                    })}
-                >
-                    Hello World
+                <div>
+                    <div
+                        className={css({
+                            bg: "red",
+                        })}
+                    >
+                        Hello World
+                    </div>
+                    <Button>aae</Button>
                 </div>
             </main>
             <footer></footer>
