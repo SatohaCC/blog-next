817f04627d5951ea26a0ab440b20ded3000f972b
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-17 01:37:31 +0900

    Revert "refactor: Simplify Header component and update Navigation to use server component"
    This reverts commit e84d2272b45eb90a1371f7e537fede76ea866461.

--- src/components/features/Header/Navigation.tsx
+++ src/components/features/Header/Navigation.tsx
@@ -1,25 +1,23 @@
-import { getMenu } from "@/lib/microcms";
-import { Suspense } from "react";
+import { HeaderProps } from "@/lib/types";
 import { css } from "styled-system/css";
 
-const Navigation = async () => {
-    const { contents } = await getMenu();
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div
-                className={css({
-                    flex: 1,
-                    display: { base: "none", md: "flex" },
-                    justifyContent: "center",
-                    fontSize: { base: "md", lg: "md" },
-                    gap: "20px",
-                    alignItems: "center",
-                })}
-            >
-                {contents?.map((content) => <div key={content.id}>{content.label}</div>)}
-            </div>
-        </Suspense>
-    );
+type Props = {
+    contents: HeaderProps;
 };
+
+const Navigation = ({ contents }: Props) => (
+    <div
+        className={css({
+            flex: 1,
+            display: { base: "none", md: "flex" },
+            justifyContent: "center",
+            fontSize: { base: "md", lg: "md" },
+            gap: "20px",
+            alignItems: "center",
+        })}
+    >
+        {contents?.map((content) => <div key={content.id}>{content.label}</div>)}
+    </div>
+);
 
 export default Navigation;
--- src/components/features/Header/header.tsx
+++ src/components/features/Header/header.tsx
@@ -1,45 +1,55 @@
 import Logo from "@/components/features/Header/Logo";
 import Navigation from "@/components/features/Header/Navigation";
+import { getMenu } from "@/lib/microcms";
 import { MENU_REVALIDATE } from "@/lib/siteInfo";
+import { HeaderProps } from "@/lib/types";
 import Image from "next/image";
 import { css } from "styled-system/css";
 
 export const revalidate = MENU_REVALIDATE;
 
-const Header = () => {
-    return (
-        <header>
+// Storybookのためにクライアントコンポーネントを作成
+const HeaderPresentation = ({ contents }: { contents: HeaderProps }) => (
+    <header>
+        <div
+            className={css({
+                position: "sticky",
+                top: 0,
+                borderBottomWidth: "1px",
+                borderBottomColor: "gray.200",
+                maxW: "1280px",
+                margin: "0 auto",
+                px: "5",
+                bg: "white",
+                zIndex: 200,
+                display: "flex",
+                alignItems: "center",
+            })}
+        >
+            <div className={css({ flex: 1 })}>
+                <Logo />
+            </div>
+            <Navigation contents={contents} />
             <div
                 className={css({
-                    position: "sticky",
-                    top: 0,
-                    borderBottomWidth: "1px",
-                    borderBottomColor: "gray.200",
-                    maxW: "1280px",
-                    margin: "0 auto",
-                    px: "5",
-                    bg: "white",
-                    zIndex: 200,
+                    flex: 1,
+                    textAlign: "right",
                     display: "flex",
+                    justifyContent: "flex-end",
                     alignItems: "center",
                 })}
             >
-                <Logo />
-                <Navigation />
-                <div
-                    className={css({
-                        flex: 1,
-                        textAlign: "right",
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                    })}
-                >
-                    <Image src="/github-mark.svg" alt="icon" width={20} height={20} />
-                </div>
+                <Image src="/github-mark.svg" alt="icon" width={20} height={20} />
             </div>
-        </header>
-    );
+        </div>
+    </header>
+);
+
+// サーバーコンポーネント
+const Header = async () => {
+    const { contents } = await getMenu();
+    return <HeaderPresentation contents={contents} />;
 };
 
+export { HeaderPresentation };
 export default Header;
