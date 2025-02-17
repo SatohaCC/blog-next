b4537a2c20425c1bdbdefa132a9415c8330384d2
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-17 23:45:12 +0900

    refactor: Update Card component to use ArticleType and improve data handling

--- src/components/ui/Card/card.tsx
+++ src/components/ui/Card/card.tsx
@@ -1,24 +1,25 @@
+import { ArticleType } from "@/lib/types";
+import dayjs from "dayjs";
+import "highlight.js/styles/a11y-dark.css";
 import Link from "next/link";
 import { cardRecipe } from "./card.recipe";
 
 type CardProps = {
-    title: string;
-    description: string;
-    date: string;
-    tags?: string[];
-    variant?: "outline" | "filled";
+    content: ArticleType;
 };
 
-const Card = ({ title, description, date, tags = [], variant }: CardProps) => {
-    const classes = cardRecipe({ variant });
+const Card = ({ content }: CardProps) => {
+    const publishedAt = dayjs(content.publishedAt).format("YYYY.MM.DD");
+    const classes = cardRecipe({});
+    const { id, title, summary, categories = [] } = content;
     return (
         <div className={classes.root}>
-            <div className={classes.date}>{date}</div>
-            <Link href={`/articles/${title}`}>
+            <div className={classes.date}>{publishedAt}</div>
+            <Link href={`/article/${id}`}>
                 <div className={classes.title}>{title}</div>
             </Link>
-            <div className={classes.description}>{description}</div>
-            <div className={classes.tags}>{tags.join(", ")}</div>
+            <div className={classes.description}>{summary}</div>
+            <div className={classes.tags}>{categories.join(", ")}</div>
         </div>
     );
 };
