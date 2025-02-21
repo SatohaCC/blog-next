01f4dab2d0532185abd1a89edb86aa508e2ecb37
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-21 21:41:21 +0900

    feat(Link): add styling and hover effects to Link component
    - Apply text decoration and outline styles
    - Implement hover color change for improved interactivity
    - Use styled-system/css for consistent styling

--- src/components/ui/Link/link.tsx
+++ src/components/ui/Link/link.tsx
@@ -1,6 +1,7 @@
 "use client";
 import NEXTLink from "next/link";
 import { Link as RACLink } from "react-aria-components";
+import { css } from "styled-system/css";
 
 type Props = {
     children: React.ReactNode;
@@ -10,7 +11,17 @@ type Props = {
 const Link = ({ children, href }: Props) => {
     return (
         <NEXTLink href={href} passHref>
-            <RACLink>{children}</RACLink>
+            <RACLink
+                className={css({
+                    textDecoration: "none",
+                    outline: "none",
+                    _hover: {
+                        color: "blue.600",
+                    },
+                })}
+            >
+                {children}
+            </RACLink>
         </NEXTLink>
     );
 };
