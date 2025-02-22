307cccc9f0d429262030cd2b5a0b2dc517a70bdb
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-22 17:14:15 +0900

    chore(assets): update favicon with new icon design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Pagination from "@/components/features/Pagination/pagination";
 import { getList } from "@/lib/microcms";
 import { PER_PAGE } from "@/lib/siteInfo";
 
-// export const dynamic = "force-static";
-
 export default async function Home() {
     const { contents, totalCount } = await getList({ limit: PER_PAGE, offset: 0 });
 
