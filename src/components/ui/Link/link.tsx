9378faec311075b0d40689705a014d9e6c8d2629
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-22 13:58:46 +0900

    feat(Link): add optional icon support to Link component
    - Extend Link component to accept an optional icon prop
    - Render icon before children when provided
    - Update component type definition to include IconType

--- src/components/ui/Link/link.tsx
+++ src/components/ui/Link/link.tsx
@@ -1,6 +1,7 @@
 "use client";
 import NEXTLink from "next/link";
 import { Link as RACLink } from "react-aria-components";
+import { IconType } from "react-icons";
 import { css } from "styled-system/css";
 
 type Props = {
@@ -8,14 +9,13 @@ type Props = {
     href: string;
     target?: string;
     rel?: string;
+    icon?: IconType;
 };
 
-const Link = ({ children, href, target = "_self", rel = "" }: Props) => {
+const Link = ({ children, href, target = "_self", rel = "", icon: Icon }: Props) => {
     return (
-        <NEXTLink href={href} passHref>
+        <NEXTLink href={href} passHref target={target} rel={rel}>
             <RACLink
-                target={target}
-                rel={rel}
                 className={css({
                     textDecoration: "none",
                     outline: "none",
@@ -24,6 +24,7 @@ const Link = ({ children, href, target = "_self", rel = "" }: Props) => {
                     },
                 })}
             >
+                {Icon && <Icon />}
                 {children}
             </RACLink>
         </NEXTLink>
