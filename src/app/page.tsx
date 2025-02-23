5291f23e7df1f2a839f41bf4e765f212cfc0f288
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2025-02-23 10:21:42 +0900

    tmp test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Pagination from "@/components/features/Pagination/pagination";
 import { getList } from "@/lib/microcms";
 import { PER_PAGE } from "@/lib/siteInfo";
 
-export const revalidate = 3600;
+export const revalidate = 3602;
 
 export default async function Home() {
     const { contents, totalCount } = await getList({ limit: PER_PAGE, offset: 0 });
